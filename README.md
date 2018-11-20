7440bb0ade4ee8eb48606c1f50b081c9554f0613
Thom Wright <github@thomwright.co.uk>
2018-11-20 21:17:40 +0000

    Add simple cmdline opt parsing

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # nest-exporter
+
+## Developing
+
+```bash
+# build
+stack build
+
+# run
+stack exec -- next-exporter --help
+
+# format source files
+hfmt -w
+```
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
 module Main where
 
-import Lib
+import           Data.Semigroup      ((<>))
+import           Lib (someFunc)
+import           Options.Applicative (Parser, ParserInfo, execParser, fullDesc,
+                                      header, help, helper, info, long, metavar,
+                                      progDesc, short, strOption)
 
 main :: IO ()
-main = someFunc
+main = run =<< execParser opts
+  where
+    run (CmdLineOptions _) = someFunc
+
+newtype CmdLineOptions = CmdLineOptions
+  { config :: FilePath
+  }
+
+optParser :: Parser CmdLineOptions
+optParser =
+  CmdLineOptions <$>
+  strOption
+    (long "config" <> short 'c' <> metavar "CONFIG_FILE" <>
+     help "Path to config file")
+
+opts :: ParserInfo CmdLineOptions
+opts =
+  info
+    (helper <*> optParser)
+    (fullDesc <> progDesc "Prometheus metrics exporter for the Nest thermostat" <>
+     header "nest-exporter - Prometheus metrics for Nest")
